7fd1885202bc96658b56605efa8bd975e96c6555
rado <hei.rado.2@gmail.com>
2024-06-07 15:47:37 +0300

    refactor: Improve code formatting for better readability

--- src/main/java/com/examen/Projet1/ConjonctionDeCoordination/CalculConjonction.java
+++ src/main/java/com/examen/Projet1/ConjonctionDeCoordination/CalculConjonction.java
@@ -5,16 +5,22 @@ import com.examen.Projet1.Affirmation.QuestionVerite;
 public class CalculConjonction {
     public boolean calcul (QuestionVerite premierAffiramtion , QuestionVerite secondAffirmation, ConjonctionsDeCoordinations conjonctionsDeCoordinations ) {
         if (conjonctionsDeCoordinations.equals("et") && premierAffiramtion.isVraiOuFaux() == true && secondAffirmation.isVraiOuFaux() == true) {
+
             return true;
+
         }
         if (conjonctionsDeCoordinations.equals("ou") && premierAffiramtion.isVraiOuFaux() || secondAffirmation.isVraiOuFaux() == true) {
+
             return true;
+
         }
         if (conjonctionsDeCoordinations.equals("donc") && premierAffiramtion.isVraiOuFaux() == true && secondAffirmation.isVraiOuFaux()== false) {
+
             if(premierAffiramtion.isVraiOuFaux()!=true){
                 return true;
             }
         }
+
         return false;
     }
 
